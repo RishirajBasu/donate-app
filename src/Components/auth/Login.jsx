06d958a54e0b7e3fdf90f224d5d5df04a99c98dc
Sayak Ghosh <sayakdeepghosh01@gmail.com>
2023-06-26 00:47:09 +0530

    2nd edit but still get error

--- src/Components/auth/Login.jsx
+++ src/Components/auth/Login.jsx
@@ -6,7 +6,7 @@ import { signinSchema } from "./Schema";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+// import { useNavigate } from  "react-router-dom";
 const Login = () => {
   const initialvalues = {
     email: "",
@@ -17,137 +17,116 @@ const Login = () => {
     useFormik({
       initialValues: initialvalues,
       validationSchema: signinSchema,
-      validateOnChange: signinSchema,
-      validateOnBlur: signinSchema,
-      onSubmit: (values, action) => {
+      onSubmit:(values,action) => {
         console.log(values);
         action.resetForm();
       },
     });
   const URL = "http://localhost:8000";
 
-  const Login = () => {
-    // this is used to navigate to different pages
-    const navigate = useNavigate();
+  const loginUser = async(values) => {
+    try {
+      // if (
 
-    const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
-      useFormik({
-        initialValues: initialvalues,
-        validationSchema: loginSchema,
-        validateOnChange: logininSchema,
-        validateOnBlur: loginSchema,
-        onSubmit: (values, action) => {
-          console.log(values);
-          action.resetForm();
+      //   values.email === "" ||
+      //   values.password === ""
+
+      // ) {console.log("hi")}
+
+      let { data } = await axios.post(
+        `${URL}/accounts/register/`,
+        {
+          email: values.email,
+          password: values.password,
         },
-      });
-
-    const URL = "http://localhost:8000";
-
-    const loginUser = async (values) => {
-      try {
-        if (
-          
-          values.email === "" ||
-          values.password === "" 
-          
-        )  
-
-        const { data } = await axios.post(
-          `${URL}/accounts/register/`,
-          {            
-            email: values.email,            
-            password: values.password,
+        {
+          headers: {
+            "Content-type": "application/json",
           },
-          {
-            headers: {
-              "Content-type": "application/json",
-            },
-          }
-        );
-
-
-      } catch (error) {
-        console.log(error);
-        toast.error("Something went wrong!");
-      }
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!");
     }
-  };
-    console.log(errors);
-    return (
-      <div>
-        <Wapper>
-          <Container_left>
-            <h1> Donate </h1>
-          </Container_left>
-          <Container_right className="container_right login">
-            <div className="form-container">
-              <div className="auth-heading">
-                <p>Log in to Donate</p>
-              </div>
-              <form onSubmit={handleSubmit} autoComplete="off">
-                <div className="Email">
-                  <label htmlFor="email" className="input-label">
-                    Email{" "}
-                  </label>
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    autoComplete="off"
-                    placeholder="Enter your Email ID"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                </div>
-                {errors.email && touched.email ? (
-                  <div className="errors">
-                    <p>{errors.email}</p>
-                  </div>
-                ) : null}
-                <div className="Password">
-                  <label htmlFor="password" className="input-label">
-                    Password{" "}
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    autoComplete="off"
-                    placeholder="Enter your Password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                </div>
-                {errors.password && touched.password ? (
-                  <div className="errors">
-                    <p>{errors.password}</p>
-                  </div>
-                ) : null}
-
-                <div className="buttons">
-                  <button className="input-button" type="submit"
-                  onClick={() =>{
-                    loginUser(values);
-                  }}
-                  >
-                    Log in
-                  </button>
-                </div>
-
-                <div className="form-bottom">
-                  Haven't registered yet? <a href="/signup">Sign up</a>
-                </div>
-              </form>
+  
+};
+console.log(errors);
+return (
+  <div>
+    <Wapper>
+      <Container_left>
+        <h1> Donate </h1>
+      </Container_left>
+      <Container_right className="container_right login">
+        <div className="form-container">
+          <div className="auth-heading">
+            <p>Log in to Donate</p>
+          </div>
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <div className="Email">
+              <label htmlFor="email" className="input-label">
+                Email{" "}
+              </label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                autoComplete="off"
+                placeholder="Enter your Email ID"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
             </div>
-          </Container_right>
-        </Wapper>
-      </div>
-    );
-  };
+            {errors.email && touched.email ? (
+              <div className="errors">
+                <p>{errors.email}</p>
+              </div>
+            ) : null}
+            <div className="Password">
+              <label htmlFor="password" className="input-label">
+                Password{" "}
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                autoComplete="off"
+                placeholder="Enter your Password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+            {errors.password && touched.password ? (
+              <div className="errors">
+                <p>{errors.password}</p>
+              </div>
+            ) : null}
 
+            <div className="buttons">
+              <button
+                className="input-button"
+                type="submit"
+                onClick={() => {
+                  loginUser(values);
+                }}
+              >
+                Log in
+              </button>
+            </div>
+
+            <div className="form-bottom">
+              Have not registered yet? <a href="/signup">Sign up</a>
+            </div>
+          </form>
+        </div>
+      </Container_right>
+    </Wapper>
+  </div>
+)
+              };
 const Wapper = styled.div`
   margin: 0px;
   border: none;
