f10561cd933550a28cffba626972973d6bab14bc
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-06-29 11:51:47 +0530

    geo location errors fixed

--- src/Components/auth/geo.jsx
+++ src/Components/auth/geo.jsx
@@ -1,42 +1,39 @@
-import React from "react";
-import './geo.css';
-import { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-function App() {
- const [lat, setLat] = useState(null);
- const [long, setLong] = useState(null);
- const geolocationAPI = navigator.geolocation;
- const getUserCoordinates = () => {
+function Geo() {
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+  const geolocationAPI = navigator.geolocation;
+  const getUserCoordinates = () => {
     if (!geolocationAPI) {
-      setError('Geolocation API is not available in your browser!')
+      alert("Geolocation API is not available in your browser!");
     } else {
-      geolocationAPI.getCurrentPosition((position) => {
-        const { coords } = position;
-        setLat(coords.latitude);
-        setLong(coords.longitude);
-      }, (error) => {
-        setError('Something went wrong getting your position!')
-      })
-    }
-    if (!geolocationAPI) {
-        setError('Geolocation API is not available in your browser!')
-      }
-      else {
-        geolocationAPI.getCurrentPosition((position) => {
+      geolocationAPI.getCurrentPosition(
+        (position) => {
           const { coords } = position;
           setLat(coords.latitude);
           setLong(coords.longitude);
         },
         (error) => {
-            setError('Something went wrong getting your position!')
-          })
-          
-          return (
-            <div className="App">
-              <p>Your coordinates are: {[lat, long]}</p>
-            </div>
-          );
-          getUserCoordinates();
-};
+          // console.log(error);
+          alert(error.message);
+        }
+      );
+    }
+  };
 
-export default geo;
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  return (
+    <div className="">
+      <p>
+        Your coordinates are: {lat}, {long}
+      </p>
+      <button onClick={getUserCoordinates}>Refresh</button>
+    </div>
+  );
+}
+
+export default Geo;
